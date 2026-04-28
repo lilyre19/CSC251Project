3cb16a28a771b6f64833e4dbe2d4bcda91322302
lilyre19 <riteesha.lingechetty@gmail.com>
2026-04-27 20:23:04 -0400

    Update Policy.java

--- Policy.java
+++ Policy.java
@@ -1,173 +1,72 @@
 public class Policy
-{  //fields
+{
    private String policyNumber;
    private String providerName;
-   private String firstName;
-   private String lastName;
-   private int age;
-   private String smokingStatus;
-   private double height;
-   private double weight;
+   private PolicyHolder policyHolder;
 
-   //constructors
-   public Policy()
-   {
-      policyNumber = "";
-      providerName = "";
-      firstName = "";
-      lastName = "";
-      age = 0;
-      smokingStatus = "";
-      height = 0;
-      weight = 0;
-   }
-   
-   public Policy(String pNumber, String pName, String fName, String lName,int a, String sStatus, double h, double w)
+   private static int policyCount = 0;
+
+   // Constructor
+   public Policy(String pNumber, String pName, PolicyHolder holder)
    {
       policyNumber = pNumber;
       providerName = pName;
-      firstName = fName;
-      lastName = lName;
-      age = a;
-      smokingStatus = sStatus;
-      height = h;
-      weight = w;
-   }
-   
-   //setters//
-
-   public void setPolicyNumber(String pNumber)
-   {
-      policyNumber = pNumber;
-   }
-   
-   public void setProviderName(String pName)
-   {
-      providerName = pName;
-   }
-   
-   public void setFirstName(String fName)
-   {
-      firstName = fName;
-   }
-   
-   public void setLastName(String lName)
-   {
-      lastName = lName;
-   }
-   
-   public void setAge(int a)
-   {
-      age = a;
-   }
-   
-   public void setSmokingStatus(String sStatus)
-   {
-      smokingStatus = sStatus;
-   }
-   
-   public void setHeight(double h)
-   {
-      height = h;
-   }
-   
-   public void setWeight(double w)
-   {
-      weight = w;
+      policyHolder = new PolicyHolder(holder); // copy for security
+      policyCount++;
    }
 
-   
-   //getters//
-   
+   // Getters
    public String getPolicyNumber()
    {
       return policyNumber;
    }
-   
+
    public String getProviderName()
    {
       return providerName;
    }
-   
-   public String getFirstName()
+
+   public PolicyHolder getPolicyHolder()
    {
-      return firstName;
+      return new PolicyHolder(policyHolder); // return copy
    }
-   
-   public String getLastName()
+
+   public static int getPolicyCount()
    {
-      return lastName;
+      return policyCount;
    }
-   
-   public int getAge()
-   {
-      return age;
-   }
-   
-   public String getSmokingStatus()
-   {
-      return smokingStatus;
-   }
-   
-   public double getHeight()
-   {
-      return height;
-   }
-   
-   public double getWeight()
-   {
-      return weight;
-   }
-   
-      
-   //Calculates the Policyholder's BMI
-   public double getBMI()
-   {
-      final double CONVFACTOR = 703;
-      
-      return (weight * CONVFACTOR) / (height * height);
-   }
-   
-   //Calculates the Policy's price
+
+   // Price calculation
    public double getPrice()
    {
       final double BASE_PRICE = 600;
       final double ADDITIONAL_FEE_AGE = 75;
       final double ADDITIONAL_FEE_SMOKING = 100;
       final double ADDITIONAL_FEE_PER_BMI = 20;
-      
+
       final int AGE_THRESHOLD = 50;
       final int BMI_THRESHOLD = 35;
-      
+
       double price = BASE_PRICE;
-      
-      if(age > AGE_THRESHOLD) //over 50 years
-         price += ADDITIONAL_FEE_AGE; //75
-         
-      if(smokingStatus.equalsIgnoreCase("smoker")) 
-         price += ADDITIONAL_FEE_SMOKING; //100
-            
-      //call the getBMI method
-      if(getBMI() > BMI_THRESHOLD) //BMI over 35
-         price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI); //additional BMI fee - 20
-         
+
+      if(policyHolder.getAge() > AGE_THRESHOLD)
+         price += ADDITIONAL_FEE_AGE;
+
+      if(policyHolder.getSmokingStatus().equalsIgnoreCase("smoker"))
+         price += ADDITIONAL_FEE_SMOKING;
+
+      if(policyHolder.getBMI() > BMI_THRESHOLD)
+         price += ((policyHolder.getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI);
+
       return price;
    }
-   
-   //Not included in the instructions but can be added...
-   /*Displays information about the Policy
-   public void displayInformation()
+
+   // toString (IMPORTANT)
+   public String toString()
    {
-      System.out.println("Policy Number: " + policyNumber);
-      System.out.println("Provider Name: " + providerName);
-      System.out.println("Policyholder's First Name: " + firstName);
-      System.out.println("Policyholder's Last Name: " + lastName);
-      System.out.println("Policyholder's Age: " + age);
-      System.out.println("Policyholder's Smoking Status (Y/N): " + smokingStatus);
-      System.out.println("Policyholder's Height: " + height + " inches");
-      System.out.println("Policyholder's Weight: " + weight + " pounds");
-      System.out.printf("Policyholder's BMI: %.2f\n", getBMI());
-      System.out.printf("Policy Price: $%.2f\n", getPrice());
+      return "Policy Number: " + policyNumber +
+             "\nProvider Name: " + providerName + "\n" +
+             policyHolder.toString() +
+             "\nPolicy Price: $" + String.format("%.2f", getPrice());
    }
-   */
 }
