ce10113cf23460f6bc606b61deb998e53a3503c2
lilyre19 <riteesha.lingechetty@gmail.com>
2026-04-27 20:23:16 -0400

    Refactor Policy creation to use PolicyHolder

--- PolicyDemo.java
+++ PolicyDemo.java
@@ -7,14 +7,11 @@ public class PolicyDemo
 {   
    public static void main(String[] args) throws IOException
    {
-      // Create file object
       File file = new File("PolicyInformation.txt");
       Scanner input = new Scanner(file);
 
-      // ArrayList to store policies
       ArrayList<Policy> policies = new ArrayList<>();
 
-      // Read file
       while (input.hasNext())
       {
          String policyNumber = input.nextLine();
@@ -22,47 +19,39 @@ public class PolicyDemo
          String firstName = input.nextLine();
          String lastName = input.nextLine();
          int age = input.nextInt();
-         input.nextLine(); // clear buffer
+         input.nextLine();
          String smokingStatus = input.nextLine();
          double height = input.nextDouble();
          double weight = input.nextDouble();
-         input.nextLine(); // clear buffer
+         input.nextLine();
 
-         // Create object
-         Policy policy = new Policy(policyNumber, providerName, firstName,
-                                    lastName, age, smokingStatus, height, weight);
+         // CREATE PolicyHolder first
+         PolicyHolder holder = new PolicyHolder(firstName, lastName, age,
+                                                smokingStatus, height, weight);
+
+         // PASS it into Policy
+         Policy policy = new Policy(policyNumber, providerName, holder);
 
          policies.add(policy);
       }
 
       input.close();
 
-      // Counters
       int smokers = 0;
       int nonSmokers = 0;
 
-      // Display all policies
       for (Policy policy : policies)
       {
-         System.out.println("Policy Number: " + policy.getPolicyNumber());
-         System.out.println("Provider Name: " + policy.getProviderName());
-         System.out.println("Policyholder's First Name: " + policy.getFirstName());
-         System.out.println("Policyholder's Last Name: " + policy.getLastName());
-         System.out.println("Policyholder's Age: " + policy.getAge());
-         System.out.println("Policyholder's Smoking Status (smoker/non-smoker): " + policy.getSmokingStatus());
-         System.out.println("Policyholder's Height: " + policy.getHeight() + " inches");
-         System.out.println("Policyholder's Weight: " + policy.getWeight() + " pounds");
-         System.out.printf("Policyholder's BMI: %.2f\n", policy.getBMI());
-         System.out.printf("Policy Price: $%.2f\n\n", policy.getPrice());
+         System.out.println(policy); // implicit toString
+         System.out.println();
 
-         // Count smokers
-         if (policy.getSmokingStatus().equalsIgnoreCase("smoker"))
+         if (policy.getPolicyHolder().getSmokingStatus().equalsIgnoreCase("smoker"))
             smokers++;
          else
             nonSmokers++;
       }
 
-      // Final output
+      System.out.println("There were " + Policy.getPolicyCount() + " Policy objects created.");
       System.out.println("The number of policies with a smoker is: " + smokers);
       System.out.println("The number of policies with a non-smoker is: " + nonSmokers);
    }
